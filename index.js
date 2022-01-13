715c3b41e5b99dc3a522d91eb45965c020926bb0
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-13 16:24:43 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+  document.querySelector('main').remove();
+
+  const newHeader = document.createElement('h1');
+  document.body.appendChild(newHeader);
+  newHeader.id = "victory";
+  newHeader.innerHTML = "<h1>osvaldo is the champion</h1>";
